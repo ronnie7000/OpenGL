89e4d272dfdef2ed4d6c9f4f4fcd244afeaebe9e
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2019-09-27 12:53:41 +0530

    Update Liang_Barskey_random_window.c

--- Liang_Barskey_random_window.c
+++ Liang_Barskey_random_window.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
-#include <GL/glut.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<GL/gl.h>
+#include<GL/glut.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
 
 void text(int x, int y, char *string)
 {
@@ -12,98 +15,31 @@ void text(int x, int y, char *string)
  glutBitmapCharacter(GLUT_BITMAP_9_BY_15, string[i]);
 }
 
-void myInit()
-{
- glClear(GL_COLOR_BUFFER_BIT);
- glPointSize(1.0);
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- gluOrtho2D(0.0, 800.0, 0.0, 800.0);
-}
+int first = 0;  
+int xi=0, yi=0, xf=0, yf=0; 
 
-typedef struct
-{
- GLint x;
- GLint y;
-} GLPOINT;
+int xmin,ymin,xmax,ymax;
+double p[4],q[4];
+int xt1;
+int yt1;
+int xt2;
+int yt2;
 
-char buffer[20];
+void delay(int number_of_seconds) 
+{ 
+ int milli_seconds = 1000 * number_of_seconds; 
+ clock_t start_time = clock(); 
+ while (clock() < start_time + milli_seconds); 
+} 
 
-void plot_point(int x,int y)
+void plot_point(GLint x, GLint y) 
 {
  glBegin(GL_POINTS);
  glVertex2i(x,y);
  glEnd();
- glFlush();
 }
 
-void Draw()
-{
-glClear(GL_COLOR_BUFFER_BIT);
-glColor3f(0.0,1.0,1.0);
-
-glBegin(GL_LINES);
-
-
-glVertex2f(400,799);
-glVertex2f(400,0);
-glVertex2f(799,400);
-glVertex2f(0,400);
-
-glVertex2f(400,799);
-glVertex2f(410,784);
-glVertex2f(400,799);
-glVertex2f(390,784);
-
-glVertex2f(400,0);
-glVertex2f(410,10);
-glVertex2f(400,0);
-glVertex2f(390,10);
-
-glVertex2f(0,400);
-glVertex2f(10,410);
-glVertex2f(0,400);
-glVertex2f(10,390);
-
-glVertex2f(799,400);
-glVertex2f(789,390);
-glVertex2f(799,400);
-glVertex2f(789,410);
-glEnd(); 
-
-text( 10,775, "Akansh Nirmal");
-text( 10,750, "500061342");
-text( 410,750," Y-Axis");
-text( 700,410," X-Axis");
-glEnd();
-glFlush();
-}
-
-int xi,xf,yi,yf;
-
-void pre_bresenham(int x1, int y1, int x2, int y2)
-{
- if(x1<x2)
- { 
-  xi = x1;
-  xf = x2;
-  yi = y1;
-  yf = y2;
-  
-  Bresenham_algo(x1,y1,x2,y2); 
- }
- else
- {
-  xi = x2;
-  xf = x1;
-  yi = y2;
-  yf = y1;
-   
-  Bresenham_algo(x2,y2,x1,y1);
- }
-}
-
-void Bresenham_algo(int x1, int y1, int x2, int y2)
+void Bresenham(int x1, int y1, int x2, int y2)
 {
  int dx,dy,Po;
  int s=0;
@@ -216,38 +152,6 @@ void Bresenham_algo(int x1, int y1, int x2, int y2)
 glFlush();
 }
 
-
-int xmin=0,xmax=0,ymin=0,ymax=0;
-double p[0],q[0];
-int xt1, yt1, xt2, yt2;
-
-void clipping_window()
-{
- int rx = (rand()%300)+200;
- int ry = (rand()%300)+200;
-
- xmin = rx;
- xmax = rx+200;
- ymin = ry;
- ymax = ry+200;
- 
- glColor3f(1.0,0.0,1.0);
- glBegin(GL_LINES);
- 
- glVertex2f(xmin,ymin);
- glVertex2f(xmax,ymin); 
- glVertex2f(xmax,ymin);
- glVertex2f(xmax,ymax);
- glVertex2f(xmax,ymax);
- glVertex2f(xmin,ymax);
- glVertex2f(xmin,ymax);
- glVertex2f(xmin,ymin);
- glEnd();
-
-int x = liang_barsky(xi,yi,xf,yf);
- glFlush();
-}
-
 int liang_barsky(int x1,int y1,int x2,int y2)
 {
  float t1=0.0,t2=1.0;
@@ -263,14 +167,11 @@ int liang_barsky(int x1,int y1,int x2,int y2)
  q[2]=y1-ymin;
  p[3]=dy;
  q[3]=ymax-y1;
- 
  for(i=0;i<4;i++)
  {
-  if(p[i]==0 && q[i]<0)
-  { 
-   glColor3f(0.0,1.0,0.0);
-   pre_bresenham(x1,y1,x2,y2);
-   return 0;
+  if(p[i]==0 && q[i]<0)  
+  {
+   return 0; 
   }
   
   if(p[i]<0.0)
@@ -281,81 +182,191 @@ int liang_barsky(int x1,int y1,int x2,int y2)
     t1=t;
    }
   }
+  
   if(p[i]>0.0)
   {  
    t=q[i]/p[i];
    if(t<t2)
    { 
-    t2=t;
+    t2=t; 
    }
   }
  }
- 
  if(t1<t2)
  {  
   xt1=x1+t1*(x2-x1);
   yt1=y1+t1*(y2-y1);
   xt2=x1+t2*(x2-x1);
   yt2=y1+t2*(y2-y1);
-     	 
+  
+  glClear(GL_COLOR_BUFFER_BIT);
+  glFlush();
+  Draw();
+  glColor3f(1.0,0.0,1.0);
+  Bresenham( xmin, ymin, xmax, ymin);
+  Bresenham( xmax, ymin, xmax, ymax);
+  Bresenham( xmin, ymax,xmax, ymax);
+  Bresenham( xmin, ymax, xmin, ymin);
+  
   glColor3f(0.0,0.0,1.0);
-  pre_bresenham( xt1, yt1, xt2, yt2);
-  glColor3f(1.0,1.0,0.0);
-  pre_bresenham( x1, y1, xt1, yt1);
-  glColor3f(1.0,1.0,0.0);
-  pre_bresenham( xt2, yt2, x2, y2);
+  Bresenham( xt1, yt1, xt2, yt2);
+  glColor3f(0.0,1.0,0.0);
+  Bresenham( xi, yi, xt1, yt1);
+  glColor3f(0.0,1.0,0.0);
+  Bresenham( xt2, yt2, xf, yf);
   return 1;
  }
  
  else
  {
-  glColor3f(0.0,1.0,0.0);
-  pre_bresenham(x1,y1,x2,y2);
-  return 0;
+  Draw();
+  glColor3f(0,1,0);
+  glFlush();
+  Bresenham(xi,yi,xf,yf);
+  
+  glColor3f(1.0,0.0,1.0);
+  Bresenham( xmin, ymin, xmax, ymin);
+  Bresenham( xmax, ymin, xmax, ymax);
+  Bresenham( xmin, ymax,xmax, ymax);
+  Bresenham( xmin, ymax, xmin, ymin);
+  return 0; 
+ 
  }
 }
 
-void Mouse(int button,int state,int x,int y)
+void clipping_window()
 {
- static int count;
- static GLPOINT coordinate[2];
- if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+ int flag=0;
+ while(flag==0)
  {
-  coordinate[count].x = x;
-  coordinate[count].y = 800 - y;
-  count++;
- }
- if(count == 2)
- { 
- glColor3f(0.0,1.0,0.0);
- pre_bresenham(coordinate[0].x, coordinate[0].y, coordinate[1].x, coordinate[1].y);
- count=0;
+  int randomX=(rand()%300)+150;
+  int randomY=(rand()%300)+150;
+  if(randomX<599&&randomX>149)
+  {
+   if(randomY<599&&randomY>149)
+   {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+	glColor3f(0.0,1.0,0.0);
+	Bresenham( xi, yi, xf, yf);
+    
+	xmin=randomX;
+    xmax=randomX+200;
+    ymin=randomY;
+    ymax=randomY+200;
+    
+    glColor3f(1.0,0.0,1.0);
+	Bresenham( xmin, ymin, xmax, ymin);
+    Bresenham( xmax, ymin, xmax, ymax);
+    Bresenham( xmin, ymax,xmax, ymax);
+    Bresenham( xmin, ymax, xmin, ymin);
+    
+	int x = liang_barsky(xi,yi,xf,yf);
+    delay(2);
+   }
+  }
  }
 }
 
-void Key(unsigned char key,int x,int y)
+void Draw()
 {
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(0.0,1.0,1.0);
+
+glBegin(GL_LINES);
+
+glVertex2f(400,799);
+glVertex2f(400,0);
+glVertex2f(799,400);
+glVertex2f(0,400);
+
+glVertex2f(400,799);
+glVertex2f(410,784);
+glVertex2f(400,799);
+glVertex2f(390,784);
+
+glVertex2f(400,0);
+glVertex2f(410,10);
+glVertex2f(400,0);
+glVertex2f(390,10);
+
+glVertex2f(0,400);
+glVertex2f(10,410);
+glVertex2f(0,400);
+glVertex2f(10,390);
+
+glVertex2f(799,400);
+glVertex2f(789,390);
+glVertex2f(799,400);
+glVertex2f(789,410);
+glEnd(); 
+
+text( 10,775, "Akansh Nirmal");
+text( 10,750, "500061342");
+text( 410,750," Y-Axis");
+text( 700,410," X-Axis");
+glEnd();
+glFlush();
+}
+
+void Mouse(int button, int state, int x, int y) 
+{
+ if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) 
+ { 
+  switch(first)  
+  {  
+   case 0:  
+   xi = x;  
+   yi = 800-y;  
+   first = 1;  
+   break;  
+   
+   case 1:  
+   xf = x;  
+   yf = 800-y;
+
+   if(xi>xf)
+   {
+    int temp=xi;
+    xi=xf;
+    xf=temp;  
+    temp=yi;
+    yi=yf;
+    yf=temp;  
+   } 
+   glColor3f(0.0,1.0,0.0);
+   Bresenham( xi, yi, xf, yf);
+   first = 2;
+   break;
+  }  
+ }  
+}  
+
+void Keyboard(unsigned char key,int x,int y)
+{
+ glClear(GL_COLOR_BUFFER_BIT);
  if(key==32)
  {
-  glClear(GL_COLOR_BUFFER_BIT);
-  Draw();
   clipping_window();
  }
 }
 
-
-int main(int argc,char** argv)
-{
- glutInit(&argc,argv);
- glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
- glutInitWindowPosition(100,100);
- glutInitWindowSize(800,800);
+void init() 
+{ 
+ glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+ glutInitWindowPosition(10,10);
+ glutInitWindowSize(800, 800);
  glutCreateWindow("Liang Barsky Algorithm");
- glutDisplayFunc(Draw);
- glutKeyboardFunc(Key);
- glutMouseFunc(Mouse);
- myInit();
- glutMainLoop();
- return 0;
+ glClear(GL_COLOR_BUFFER_BIT);
+ gluOrtho2D(0,800,0,800); 
 }
-
+int main(int argc, char **argv) 
+{
+  glutInit(&argc, argv);
+  init();
+  glutDisplayFunc(Draw);
+  glutMouseFunc(Mouse); 
+  glutKeyboardFunc(Keyboard); 
+  glutMainLoop();
+  return 0;
+}
